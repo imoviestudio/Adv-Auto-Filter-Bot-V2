6de3d613b39601906385597fa11ab2bc030cae71
M-STER <86182000+M-STER-YT@users.noreply.github.com>
2021-06-21 05:26:03 -0700

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -99,7 +99,7 @@ async def start(bot, update):
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await update.reply_sticker(sticker="CAACAgUAAxkBAAITMWDPagTnlfu9TY_r3hi7X1kTlMDFAAIVAQACyJRkFGZEMKKnFWwTHgQ")
+    await update.reply_sticker(sticker="CAACAgEAAxkBAAJEemDQhN67WfA0jR_5ftZStaRMR20YAALKAAN-3IBGwOBvi-NZUuMeBA")
 
 
 @Client.on_message(filters.command(["help"]) & filters.private, group=1)
@@ -131,11 +131,4 @@ async def about(bot, update):
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.ABOUT_TEXT,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
+    await update.reply_sticker(sticker="CAACAgEAAxkBAAJEemDQhN67WfA0jR_5ftZStaRMR20YAALKAAN-3IBGwOBvi-NZUuMeBA")
